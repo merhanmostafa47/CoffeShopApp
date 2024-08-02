fa2cf5a5aa70a7d11dda9065e1756e0a8d880953
Merhan Mostafa <merhanmostafa47@gmail.com>
2024-08-02 16:15:13 +0300

    Make navbar

---
+++ components/navigations/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="relative">
+    <UContainer>
+      <div class="flex items-center justify-between py-6">
+        <NavbarLogo />
+
+        <NavbarActionButtons />
+      </div>
+    </UContainer>
+  </nav>
+</template>
---
+++ components/navigations/navbarModules/NavbarLogo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="logo_wrapper">
+    <NuxtLink to="/">
+      <NuxtImg
+        :placeholder="[60, 8]"
+        src="/images/logo.svg"
+        width="47"
+        height="35"
+        fit="contain"
+        quality="80"
+        alt="logo"
+        title="Coffee shop logo"
+      />
+    </NuxtLink>
+  </div>
+</template>
