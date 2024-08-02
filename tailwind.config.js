95a42930b93004c12c0d0c9fab98b737714311f3
Merhan Mostafa <merhanmostafa47@gmail.com>
2024-08-02 14:50:44 +0300

    Setup main modules and styes

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+    "./app.config.ts",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // Main colors
+        "main-clr": "#004876",
+        "secondary-clr": "#3B2F2F",
+        "dark-secondary-clr": "#292625",
+        "light-secondary-clr": "#6F4336",
+
+        // Backgrounds colors
+        "main-bg": "#6F4336",
+        "border-clr": "#6F4336",
+
+        // Text colors
+        "default-clr": "#3B2F2F",
+        "grey-clr": "#8B8BA5",
+        "light-clr": "#FFFFFF95",
+      },
+    },
+  },
+};
